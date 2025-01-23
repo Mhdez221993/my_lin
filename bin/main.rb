3486baa75a3a2fedb0a8041174278eb5b16fd1d9
Moises Hernandez <mhdezcoronado@gmail.com>
2025-01-23 20:10:58 +0000

    Refactor main.rb to improve file inspection and result reporting

--- bin/main.rb
+++ bin/main.rb
@@ -6,19 +6,34 @@ require_relative '../lib/empty_file'
 require_relative '../lib/trailing_space'
 require_relative '../lib/indentation'
 
-class_offense = Offenses.new
-files = ARGV.empty? ? Dir["**/*\.rb"] : ARGV
-puts " Inspecting #{files.size} files"
-puts "\n"
-puts ' Offenses:'
-files.each do |file|
-  EmptyFile.new(file, class_offense)
-  TrailingSpace.new(file, class_offense)
-  Indentation.new(file, class_offense)
+INSPECTING_MSG = " Inspecting %<file_count>d files".freeze
+OFFENSES_MSG = ' Offenses:'.freeze
+RESULT_MSG = " %<file_count>d files inspected, %<offense_count>s detected".freeze
+
+def inspect_files(files, class_offense)
+  files.each do |file|
+    EmptyFile.new(file, class_offense)
+    TrailingSpace.new(file, class_offense)
+    Indentation.new(file, class_offense)
+  end
 end
 
-puts "\n"
-num = class_offense.count_offenses
-class_offense.print_offenses
-puts "\n"
-puts " #{files.size} files inspected, #{num.positive? ? num.to_s + ' offense'.red : 'no offense'.green} detected"
+def print_results(files, class_offense)
+  num = class_offense.count_offenses
+  class_offense.print_offenses
+  puts "\n"
+  puts format(RESULT_MSG, file_count: files.size, offense_count: num.positive? ? "#{num} offense".red : 'no offense'.green)
+end
+
+def main
+  class_offense = Offenses.new
+  files = ARGV.empty? ? Dir["**/*.rb"] : ARGV
+  puts format(INSPECTING_MSG, file_count: files.size)
+  puts "\n"
+  puts OFFENSES_MSG
+  inspect_files(files, class_offense)
+  puts "\n"
+  print_results(files, class_offense)
+end
+
+main if __FILE__ == $PROGRAM_NAME
